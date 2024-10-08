2a6fc63c0768e1e68f06d932548ccaa5c05b82cd
asv-spb <asvdevpro@gmail.com>
2024-10-08 20:00:02 +0300

    remove debugger from server

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def search():
     return Response(generate(), content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
